94feca43aa52582dd1e9c8c37c0b975ee103ca05
pratik <pmgajjar25@gmail.com>
2022-01-12 02:19:49 +0530

    Complete struct excercise

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,16 +1,27 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Quit,
+    Echo { msg: String },
+    Move { x: i32, y: i32 },
+    ChangeColor {r: i32, g: i32, b: i32 }
 }
 
 impl Message {
     fn call(&self) {
         println!("{:?}", &self);
+    }
+
+    fn Echo(&self, msg: &str) {
+        // println!("{:?}", msg);
+    }
+
+    fn ChangeColor(&self, r :i32, g :i32, b :i32) {
+        // println!("{:?}", r, g, b);
     }
 }
 
--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,13 +1,15 @@
 // structs1.rs
 // Address all the TODOs to make the tests pass!
-
-// I AM NOT DONE
+// Execute `rustlings hint structs1` for hints!
 
 struct ColorClassicStruct {
-    // TODO: Something goes here
+    // Create struct with name and hex type
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+// Create color tuple struct
+struct ColorTupleStruct(String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -19,7 +21,10 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: "green".to_string(),
+            hex: "#00FF00".to_string(),
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -28,7 +33,10 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct(
+            "green".to_string(),
+            "#00FF00".to_string(),
+        );
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -37,7 +45,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct{};
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
