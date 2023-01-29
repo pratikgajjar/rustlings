8cef76732dfc9a7d96bc07e035fa4c0bc909fd08
Pratik Gajjar <pratik@fampay.in>
2023-01-30 00:55:32 +0530

    Finish lifetimes, quiz and tests

--- exercises/lifetimes/lifetimes1.rs
+++ exercises/lifetimes/lifetimes1.rs
@@ -7,9 +7,8 @@
 //
 // Execute `rustlings hint lifetimes1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
@@ -23,4 +22,5 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is '{}'", result);
+    println!("{},{}", string1, string2);
 }
